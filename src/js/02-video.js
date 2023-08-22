8a92a8ee3c50f38d353985d351130c8d2dbc7f61
daniil0040 <danyapolishchuk777@gmail.com>
2023-08-22 18:27:58 -0500

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+        const onCurrentTime = function(data) {
+            const currentTime = data.seconds
+            localStorage.setItem("videoplayer-current-time",currentTime)
+        }
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log("uncorrect time");
+            break;
+
+        default:
+            console.log("error 404");
+            break;
+    }
+});
